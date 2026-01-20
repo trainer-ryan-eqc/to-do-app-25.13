0133e3b7694cf157f8112b50dd51bda7999153f1
Ryan <regulus@regulusdigital.com>
2026-01-20 23:00:02 +1100

    changed backend url after deployment onto render

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,6 +1,7 @@
 // ------------------------------------------------------------------- GLOBAL VARIABLES -------------------------------------------------------------------
 
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = "https://to-do-app-25-13.onrender.com";
 const taskForm = document.getElementById("taskForm");
 const toDoList = document.getElementById("toDoList");
 const completedList = document.getElementById("completedList");
